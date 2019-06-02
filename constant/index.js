d16cd3dbad00f938b219a0a08eb9b2c8c2fb6959
songlr <songlr@mingyuanyun.com>
2019-06-02 20:50:01 +0800

    fix 常量配置

--- constant/index.js
+++ constant/index.js
@@ -2,7 +2,7 @@ var HOSTS = {
     dev: 'https://paas-dev.mypaas.com.cn',
     test: 'https://paas-test.mypaas.com.cn',
     beta: 'https://paas-beta.mypaas.com.cn',
-    test: 'https://paas.mypaas.com.cn'
+    prod: 'https://paas.mypaas.com.cn'
 }
 
 module.exports = {
